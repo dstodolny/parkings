c5d133730367a9e31ce6cecedff2c32bf9396f7c
Dominik Stodolny <d.stodolny@gmail.com>
2015-02-23 13:45:27 +0100

    [#10] add load method

--- basics/example.rb
+++ basics/example.rb
@@ -61,4 +61,28 @@ class ArticlesFileSystem
       File.open(@dir + "/" + file_name, "w") { |file| file.write(file_body) }
     end
   end
+
+  def load
+    Dir.glob(@dir + "/*.article").map do |file_name|
+      load_article(file_name)
+    end
+  end
+
+  private
+
+  def load_article(file_name)
+    title = get_title(file_name)
+
+    contents = File.read(file_name).split("||")
+
+    article = Article.new(title, contents[3], contents[0])
+    article.likes = contents[1].to_i
+    article.dislikes = contents[2].to_i
+
+    article
+  end
+
+  def get_title(file_name)
+    file_name.match(/(\w+)\.article$/)[1].gsub("_", " ").capitalize
+  end
 end
