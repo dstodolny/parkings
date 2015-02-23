6bc5b44ab806073f8ac8923253ba37d534378171
Dominik Stodolny <d.stodolny@gmail.com>
2015-02-23 10:45:23 +0100

    [#5] add constructor to Article class

--- basics/example.rb
+++ basics/example.rb
@@ -2,9 +2,12 @@ class Article
   attr_accessor :likes, :dislikes
   attr_reader :title, :body, :author, :created_at
 
-  def initialize(title, body, author)
+  def initialize(title, body, author = nil)
     @title = title
     @body = body
     @author = author
+    @created_at = Time.now
+    @likes = 0
+    @dislikes = 0
   end
 end
