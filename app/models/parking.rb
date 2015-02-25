67dfd926c1164172dc9b14b095b16bbf127c6e57
Dominik Stodolny <d.stodolny@gmail.com>
2015-02-25 17:59:17 +0100

    [#30] add validations to Parking

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -2,4 +2,8 @@ class Parking < ActiveRecord::Base
   belongs_to :address
   belongs_to :owner, :class_name => "Person"
   has_many :place_rents
+  validates :places, presence: true
+  validates :hour_price, presence: true, numericality: true
+  validates :day_price, presence: true, numericality: true
+  validates :kind, inclusion: { in: %w(outdoor indoor private street) }
 end
--- test/models/parking_test.rb
+++ test/models/parking_test.rb
@@ -1,7 +1,53 @@
 require 'test_helper'
 
 class ParkingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @parking = Parking.new(places: 5, kind: "outdoor", hour_price: 10, day_price: 100)
+  end
+
+  test "should be valid" do
+    assert @parking.valid?
+  end
+
+  test "places should be present" do
+    @parking.places = nil
+    assert_not @parking.valid?
+  end
+
+  test "hour price should be present" do
+    @parking.hour_price = nil
+    assert_not @parking.valid?
+  end
+
+  test "hour price should be numerical" do
+    @parking.hour_price = "abcd"
+    assert_not @parking.valid?
+  end
+
+  test "day price should be present" do
+    @parking.day_price = nil
+    assert_not @parking.valid?
+  end
+
+  test "day price should be numerical" do
+    @parking.day_price = "abcd"
+    assert_not @parking.valid?
+  end
+
+  test "kind validation should accept valid kinds" do
+    valid_kinds = %w(outdoor indoor private street)
+    valid_kinds.each do |valid_kind|
+      @parking.kind = valid_kind
+      assert @parking.valid?
+    end
+  end
+
+  test "kind validation should reject invalid kinds" do
+    invalid_kinds = %w(single loremipsum 123456)
+    invalid_kinds.each do |invalid_kind|
+      @parking.kind = invalid_kind
+      assert_not @parking.valid?
+    end
+  end
 end
