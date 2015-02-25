80e1225f04a942e2a49eae47a2bd5d01267dec26
Dominik Stodolny <d.stodolny@gmail.com>
2015-02-25 19:42:45 +0100

    [#27] refactor validations

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,5 @@
 class Address < ActiveRecord::Base
   has_one :parking
-  validates :city, presence: true
-  validates :street, presence: true
-  validates :zip_code, presence: true, format: { with: /\A[\d]{2}-[\d]{3}\z/ }
+  validates_presence_of :city, :street, :zip_code
+  validates :zip_code, format: { with: /\A[\d]{2}-[\d]{3}\z/ }
 end
