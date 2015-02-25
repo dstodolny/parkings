300a976351366dcd6e6218aaca9b3ed112e1931b
Dominik Stodolny <d.stodolny@gmail.com>
2015-02-25 19:29:40 +0100

    [#31] add validations to PlaceRent

--- app/models/place_rent.rb
+++ app/models/place_rent.rb
@@ -1,4 +1,5 @@
 class PlaceRent < ActiveRecord::Base
   belongs_to :parking
   belongs_to :car
+  validates_presence_of :starts_at, :ends_at, :parking, :car
 end
--- test/models/place_rent_test.rb
+++ test/models/place_rent_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class PlaceRentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @place_rent = place_rents(:one)
+  end
+
+  test "is valid with proper data" do
+    assert @place_rent.valid?
+  end
+
+  test "is invalid without starts_at" do
+    @place_rent.starts_at = ""
+    @place_rent.valid?
+    assert @place_rent.errors.messages.keys.include?(:starts_at)
+  end
+
+  test "is invalid without ends_at" do
+    @place_rent.ends_at = ""
+    @place_rent.valid?
+    assert @place_rent.errors.messages.keys.include?(:ends_at)
+  end
+
+  test "is invalid without parking" do
+    @place_rent.parking = nil
+    @place_rent.valid?
+    assert @place_rent.errors.messages.keys.include?(:parking)
+  end
+
+  test "is invalid without car" do
+    @place_rent.car = nil
+    @place_rent.valid?
+    assert @place_rent.errors.messages.keys.include?(:car)
+  end
 end
