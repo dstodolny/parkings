3dab5be3d82b4cd69153f87d1508031ffb28c6d8
Dominik Stodolny <d.stodolny@gmail.com>
2015-02-25 17:13:48 +0100

    [#28] add validations to Car

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,7 @@
 class Car < ActiveRecord::Base
   belongs_to :owner, :class_name => "Person"
   has_many :place_rents
+  validates :registration_number, presence: true
+  validates :model, presence: true
+  validates :owner, presence: true
 end
--- test/models/car_test.rb
+++ test/models/car_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class CarTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    owner = Person.new(first_name: "Dominik", last_name: "Stodolny")
+    @car = Car.new(registration_number: "AAA1000", model: "Tesla Roadster", owner: owner)
+  end
+
+  test "should be valid" do
+    assert @car.valid?
+  end
+
+  test "registration number should be present" do
+    @car.registration_number = ""
+    assert_not @car.valid?
+  end
+
+  test "model should be present" do
+    @car.model = ""
+    assert_not @car.valid?
+  end
+
+  test "owner should be present" do
+    @car.owner = nil
+    assert_not @car.valid?
+  end
 end
