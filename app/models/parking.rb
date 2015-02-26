7318c43aa42d90c5c92983fc32fe51b9a9f86ea2
Dominik Stodolny <d.stodolny@gmail.com>
2015-02-26 19:08:28 +0100

    [#32] fix displaying a city

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -4,6 +4,7 @@ class Parking < ActiveRecord::Base
   belongs_to :address
   belongs_to :owner, class_name: "Person"
   has_many :place_rents
+
   validates_presence_of :places, :hour_price, :day_price
   validates_numericality_of :hour_price, :day_price
   validates :kind, inclusion: { in: KINDS }
