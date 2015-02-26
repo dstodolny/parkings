4f973fc8cc360b1811c0545cdab3eba55d628aa6
Dominik Stodolny <d.stodolny@gmail.com>
2015-02-26 12:52:49 +0100

    [#33] refactor owner and address display

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,4 +2,8 @@ class Person < ActiveRecord::Base
   has_many :parkings, :foreign_key => "owner_id"
   has_many :cars, :foreign_key => "owner_id"
   validates :first_name, presence: true
+
+  def full_name
+    [first_name, last_name].join(' ')
+  end
 end
