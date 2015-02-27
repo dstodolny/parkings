4558a221ecacdd643ff955ff827d23ce4dc72dbc
Dominik Stodolny <d.stodolny@gmail.com>
2015-02-27 13:11:21 +0100

    [#42] add place rent creation feature

--- app/controllers/place_rents_controller.rb
+++ app/controllers/place_rents_controller.rb
@@ -1,9 +1,32 @@
 class PlaceRentsController < ApplicationController
   def index
-    @place_rents = current_user.place_rents.all
+    @place_rents = current_person.place_rents.all
   end
 
   def show
-    @place_rent = current_user.place_rents.find(params[:id])
+    @place_rent = current_person.place_rents.find(params[:id])
+  end
+
+  def new
+    @cars = current_person.cars
+    @place_rent = parking.place_rents.build
+  end
+
+  def create
+    @place_rent = parking.place_rents.build(place_rent_params)
+
+    if @place_rent.save
+      redirect_to @place_rent
+    else
+      render "new"
+    end
+  end
+
+  def place_rent_params
+    params.require(:place_rent).permit(:starts_at, :ends_at, :car_id)
+  end
+
+  def parking
+    @parking ||= Parking.find(params[:parking_id])
   end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,6 @@
 class Person < ActiveRecord::Base
-  has_many :parkings, :foreign_key => "owner_id"
-  has_many :cars, :foreign_key => "owner_id"
+  has_many :parkings, foreign_key: "owner_id"
+  has_many :cars, foreign_key: "owner_id"
   has_many :place_rents, through: :cars
   validates :first_name, presence: true
 
