7a315973a2434d6f443fa51dad8faddc338f71a5
Dominik Stodolny <d.stodolny@gmail.com>
2015-03-03 10:37:50 +0100

    [#59] display full name of current person

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,6 +5,6 @@ class Person < ActiveRecord::Base
   validates :first_name, presence: true
 
   def full_name
-    [first_name, last_name].join(' ')
+    last_name.nil? ? first_name : [first_name, last_name].join(' ')
   end
 end
