6b38487ff31bd46dd18d588da15afece2ea5a7b1
Dominik Stodolny <d.stodolny@gmail.com>
2015-03-04 12:43:26 +0100

    [#68] add permalinks for cars

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,4 +2,12 @@ class Car < ActiveRecord::Base
   belongs_to :owner, :class_name => "Person"
   has_many :place_rents
   validates_presence_of :registration_number, :model, :owner
+
+  def permalink
+    "#{id}-#{model.parameterize}"
+  end
+
+  def to_param
+    permalink
+  end
 end
