26e8db24fdd504378a03d727a7235417748e5ad8
Dominik Stodolny <d.stodolny@gmail.com>
2015-03-04 20:31:52 +0100

    [#76] repair locale_from_header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,6 +23,6 @@ class ApplicationController < ActionController::Base
   end
 
   def locale_from_header
-    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    request.env['HTTP_ACCEPT_LANGUAGE'].try(:scan, /^[a-z]{2}/).try(:first)
   end
 end
