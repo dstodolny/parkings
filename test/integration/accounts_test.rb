bb19174b768c1e3386887abaf7226e5360c40dab
Dominik Stodolny <d.stodolny@gmail.com>
2015-03-04 10:39:58 +0100

    [#65] add integration tests to account creation

---
+++ test/integration/accounts_test.rb
@@ -0,0 +1,66 @@
+require 'test_helper'
+require 'capybara/rails'
+
+class AccountsTest < ActionDispatch::IntegrationTest
+
+  test "user opens account registration form" do
+    visit "/accounts/new"
+
+    assert has_content? "New Account"
+  end
+
+  test "user can create a new account" do
+    visit "/accounts/new"
+
+    fill_in "account_email", with: "test@test.pl"
+    fill_in "account_password", with: "123456"
+    fill_in "account_password_confirmation", with: "123456"
+    fill_in "account_person_attributes_first_name", with: "John"
+    fill_in "account_person_attributes_last_name", with: "Doe"
+
+    click_button "Create Account"
+
+    assert has_content? "Account has been successfully created."
+  end
+
+  test "user can't create an account with wrong password confirmation" do
+    visit "/accounts/new"
+
+    fill_in "account_email", with: "test@test.pl"
+    fill_in "account_password", with: "123456"
+    fill_in "account_password_confirmation", with: "blablabla"
+    fill_in "account_person_attributes_first_name", with: "John"
+    fill_in "account_person_attributes_last_name", with: "Doe"
+
+    click_button "Create Account"
+
+    assert has_content? "error"
+  end
+
+  test "user can't create an account with duplicate email" do
+    visit "/accounts/new"
+
+    fill_in "account_email", with: "d.stodolny@gmail.com"
+    fill_in "account_password", with: "123456"
+    fill_in "account_password_confirmation", with: "123456"
+    fill_in "account_person_attributes_first_name", with: "John"
+    fill_in "account_person_attributes_last_name", with: "Doe"
+
+    click_button "Create Account"
+
+    assert has_content? "error"
+  end
+
+  test "user can't create an account without providing the email" do
+    visit "/accounts/new"
+
+    fill_in "account_password", with: "123456"
+    fill_in "account_password_confirmation", with: "123456"
+    fill_in "account_person_attributes_first_name", with: "John"
+    fill_in "account_person_attributes_last_name", with: "Doe"
+
+    click_button "Create Account"
+
+    assert has_content? "error"
+  end
+end
