2bfb378f7db2b50dc89bcac4f6ccc160c2c1405e
Dominik Stodolny <d.stodolny@gmail.com>
2015-03-07 16:34:47 +0100

    [#88] add routes for Facebook authentication

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
   get "login", to: "sessions#new", as: :login
   delete "logout", to: "sessions#destroy", as: :logout
 
+  get "/auth/:provider/callback", to: "sessions#create"
+  get "/auth/failure", to: "sessions#failure"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
