50a3f0a026526f10ad12ede6e671c0ae63724d73
Dominik Stodolny <d.stodolny@gmail.com>
2015-03-17 00:11:02 +0100

    [#89] change facebook sign-in link to route

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
 
   get "/auth/:provider/callback", to: "sessions#create"
   get "/auth/failure", to: "sessions#failure"
+  get "/auth/facebook", as: :facebook_auth
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
